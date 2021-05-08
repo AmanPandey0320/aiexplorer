30d2bbf406444ea8e7fb5791105354b69a57c2f2
AmanPandey0320 <72158086+AmanPandey0320@users.noreply.github.com>
2021-05-09 05:08:33 +0530

    file tree added styls and actions remainig

--- src/components/fileTree.js
+++ src/components/fileTree.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import useStyles from '../styles/filetree.style'
+import useStyles from '../styles/filetree.style';
+import TreeView from '@material-ui/lab/TreeView';
+import TreeItem from '@material-ui/lab/TreeItem';
+import Icons from '../styles/icons';
+import FolderNode from './folder.node';
+const pathModule = window.require('path')
+
 
 const FileTree = (props) => {
+    const {files, onBack, onOpen, path} = props;
     const classes = useStyles();
     return ( 
-        <div className={classes.main} >
-            file tree
-        </div>
+        <TreeView
+            className={classes.tree}
+            defaultCollapseIcon={Icons.tree.folderOpen}
+            defaultExpandIcon={Icons.tree.folderClose}
+            multiSelect
+        >
+            {
+                files.map(({name, directory, size,type},index) => {
+                    if(directory){
+                        return(
+                            <TreeItem key={index} nodeId={index} label={name}>
+                                <FolderNode root={name} path={pathModule.join(path,name)}/>
+                            </TreeItem>
+                        )
+                    }else{
+                        return <TreeItem key={index} nodeId={index} label={name}/>
+                    }
+                })
+            }
+
+        </TreeView>
      );
 }
  
