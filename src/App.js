028c4026aa98ad0dc56685f07441b7cb485480da
amit-k-umar <61649790+amit-k-umar@users.noreply.github.com>
2021-05-08 19:36:14 +0530

    able to open files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, {useState, useMemo} from 'react';
 import { FilesViewer } from './FilesViewer';
-const fs = window.require('fs')
+const fs = window.require('fs-extra')
+const mime=window.require('mime')
 const pathModule = window.require('path')
 const {app} = window.require('@electron/remote');
 
@@ -14,12 +15,13 @@ const formatSize = (size) => {
 
 function App() {
   const [path, setPath] = useState(app.getAppPath());
-  const files = useMemo(()=>fs.readdirSync(path).map(file => {
+  const files = useMemo(()=>fs.readdirSync(path).map((file) => {
     const stats = fs.statSync(pathModule.join(path, file))
     return {
       name: file,
-      size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
-      directory: stats.isDirectory(),
+       size: stats.isFile() ? formatSize(stats.size ?? 0) : null,
+       directory: stats.isDirectory(),
+       type:mime.getType(file)
     }
   })
   .sort((a,b)=>{
@@ -27,7 +29,8 @@ function App() {
       return a.name.localeCompare(b.name)
     }
     return a.directory ? -1:1
-  }),[path])
+  })
+  ,[path])
 
   const onBack = () => {
     setPath(pathModule.dirname(path))
@@ -37,12 +40,12 @@ function App() {
   }
 
   const [searchString, setSearchString] = useState('');
-  const filteredFiles = files.filter(s=>s.name.startsWith(searchString))
+  const filteredFiles = files ?files.filter(s=>s.name.startsWith(searchString)):files;
   return (
     <div className="App">
       {path}
       <input value={searchString} onChange={e => setSearchString(e.target.value)}/>
-      <FilesViewer files={filteredFiles} onBack={onBack} onOpen={onOpen}/>
+      <FilesViewer files={filteredFiles} path={path} onBack={onBack} onOpen={onOpen}/>
     </div>
   );
 }
--- src/FilesViewer.js
+++ src/FilesViewer.js
@@ -1,15 +1,29 @@
-export const FilesViewer = ({files, onBack, onOpen}) => {
+const electron = window.require('electron');
+const shell = electron.shell;
+
+
+
+
+export const FilesViewer = ({files,path, onBack, onOpen}) => {
+    const openFile=(nome)=>{
+       try{ shell.openPath(path+'/'+ 
+       nome);}
+       catch(err){
+           console.log(err);
+       };
+     }
     return (<table>
         <tbody>
             <tr onClick={onBack}>
                 <td>Back</td>
             </tr>
             {
-                files.map(({name, directory, size})=> {
+                files.map(({name, directory, size,type})=> {
                     return (
-                        <tr onClick={()=>directory && onOpen(name)}>
+                        <tr onClick={()=>directory?onOpen(name):openFile(name)}>
                             <td>{name}</td>
                             <td>{size}</td>
+                           { !directory && <td>{type}</td>}
                         </tr>
                     )
                 })
