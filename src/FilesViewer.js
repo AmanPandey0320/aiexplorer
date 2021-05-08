3d7b210f188678ea6748ddcbbdb19932ff9cf9b3
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2021-05-08 17:36:25 +0530

    changes

--- src/FilesViewer.js
+++ src/FilesViewer.js
@@ -1,4 +1,19 @@
 export const FilesViewer = ({files, onBack, onOpen}) => {
+
+    const openFile = (name) => {
+        alert(name)
+    }
+    const handleClick = (e,name,directory) => {
+
+        alert('click type',e.type)
+        if(e.type === 'click') {
+            alert('left click')
+            directory? onOpen(name):openFile(name)
+        }
+        else if(e.type === 'contextmenu'){
+            alert('right click')
+        }
+    }
     return (<table>
         <tbody>
             <tr onClick={onBack}>
@@ -7,7 +22,7 @@ export const FilesViewer = ({files, onBack, onOpen}) => {
             {
                 files.map(({name, directory, size})=> {
                     return (
-                        <tr onClick={()=>directory && onOpen(name)}>
+                        <tr onClick={(e)=>handleClick(e,name, directory)}>
                             <td>{name}</td>
                             <td>{size}</td>
                         </tr>
